3eb548a4f0963716b4cefc84f88ac0a34a869a67
berryjerrycherry <berryjerrycherry123@gmail.com>
2021-12-28 08:48:37 +1100

    added a close button

--- src/scripts/utilitiespage.js
+++ src/scripts/utilitiespage.js
@@ -1,5 +1,9 @@
 document.getElementById("button-text").onclick = function() {loadPage()};
+document.getElementById("closebutton").onclick = function() {closePage()};
 
 function loadPage() {
   document.getElementById("note").style.display = "block"
+}
+function closePage() {
+  document.getElementById("note").style.display = "none"
 }
