f6246283f9da6154df0c6a557215a2d2e0f1d7c7
berryjerrycherry <berryjerrycherry123@gmail.com>
2021-12-30 09:49:08 +1100

    save button fixed

--- src/scripts/utilitiespage.js
+++ src/scripts/utilitiespage.js
@@ -1,5 +1,6 @@
 document.getElementById("button-text").onclick = function() {loadPage()};
 document.getElementById("closebutton").onclick = function() {closePage()};
+document.getElementById("save-button").onclick = function() {saveText()};
 
 function loadPage() {
   document.getElementById("note").style.display = "block"
@@ -8,14 +9,15 @@ function closePage() {
   document.getElementById("note").style.display = "none"
 }
 
-let maintextbox = document.querySelector("#maintextbox")
-
+function saveText() {
+let maintextbox = document.getElementById("maintextbox").value;
+localStorage.setItem("notes", maintextbox)
+}
 maintextbox.value = localStorage.getItem("notes")
 
-let cancel
-maintextbox.addEventListener("keyup", event => {
-  if (cancel) clearTimeout(cancel)
-  cancel = setTimeout(() => {
-    localStorage.setItem("notes", event.target.value)
-  }, 1000)
-})
+function rememberSave() {
+  let length = maintextbox.value.length
+  if (length > 0) {
+    console.log()
+  }
+}
