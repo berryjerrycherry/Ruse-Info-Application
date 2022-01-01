1deb13e0aeafde6685ff145188354dc941adefca
berryjerrycherry <berryjerrycherry123@gmail.com>
2022-01-01 15:51:12 +1100

    fixed indentation and rememberSave function fully works now

--- src/scripts/utilitiespage.js
+++ src/scripts/utilitiespage.js
@@ -1,8 +1,18 @@
-document.getElementById("button-text").onclick = function() {loadPage()};
-document.getElementById("closebutton").onclick = function() {closePage()};
-document.getElementById("back-button").onclick = function() {rememberSave()};
-document.getElementById("closebutton2").onclick = function() {closePage2()};
-document.getElementById("yes").onclick = function() {continueExit()};
+document.getElementById('button-text').onclick = function() {
+  loadPage()
+};
+document.getElementById('closebutton').onclick = function() {
+  closePage()
+};
+document.getElementById('back-button').onclick = function() {
+  rememberSave()
+};
+document.getElementById('closebutton2').onclick = function() {
+  closePage2()
+};
+document.getElementById('yes').onclick = function() {
+  continueExit()
+};
 let btnClicked = false
 
 let button = document.getElementById('save-button')
@@ -11,37 +21,41 @@ button.onclick = () => {
   btnClicked = true
   saveText()
 }
-
-
+let hasChanged = false
+let changed = document.getElementById('maintextbox')
+changed.addEventListener("input", () => {
+  hasChanged = true
+})
 
 function closePage2() {
-  document.getElementById("warningmessage").style.display = "none"
+  document.getElementById("warningmessage").style.display = 'none'
 }
 
 
 function loadPage() {
-  document.getElementById("note").style.display = "block"
+  document.getElementById("note").style.display = 'block'
 }
+
 function closePage() {
-  document.getElementById("note").style.display = "none"
+  document.getElementById("note").style.display = 'none'
 }
 
 function saveText() {
-    let maintextbox = document.getElementById("maintextbox").value;
-    localStorage.setItem("notes", maintextbox)
+  let maintextbox = document.getElementById("maintextbox").value;
+  localStorage.setItem('notes', maintextbox)
 }
 maintextbox.value = localStorage.getItem("notes")
 
 function rememberSave() {
-  let length = document.getElementById("maintextbox").value.length;
-  if (btnClicked == true){
-  history.go(-1);
-  }
-  else if (length == 0){
-    history.go(-1);
-  }
-  else if (length > 0 && btnClicked == false) {
-    document.getElementById("warningmessage").style.display = "block"
+  let length = document.getElementById('maintextbox').value.length;
+  if (btnClicked == true) {
+      history.go(-1);
+  } else if (length == 0) {
+      history.go(-1);
+  } else if (hasChanged == false) {
+      history.go(-1);
+  } else if (length > 0 && btnClicked == false) {
+      document.getElementById("warningmessage").style.display = 'block'
   }
 }
 
